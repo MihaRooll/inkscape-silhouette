2691ccd8a651c5e764c7812e8961154aa768b513
MihaRooll <109861827+MihaRooll@users.noreply.github.com>
2025-08-11 15:36:07 +0300

    Add plotter manager for Silhouette devices

--- silhouette/__init__.py
+++ silhouette/__init__.py
@@ -1,1 +1,8 @@
+"""Silhouette helper package."""
+
 # __init__ is needed for import statements across subdirectories.
+
+from .plotter_manager import PlotterManager
+
+__all__ = ["PlotterManager"]
+
